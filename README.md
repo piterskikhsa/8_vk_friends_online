227ea41b31a31a82f55c4a8059f07215b8032a50
Sergey Piterskikh <piterskikhsa@mail.ru>
2017-08-06 00:42:50 +0300

    The functions get user_login and get_user password were created and get_online_friends function was updated

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+The script displays the first name, the second name and id of the users who are on the site.
 
 # How to Install
 
@@ -8,6 +8,14 @@ Python 3 should be already installed. Then use pip (or pip3 if there is a confli
 
 ```bash
 pip install -r requirements.txt # alternatively try pip3
+
+python vk_friends_online.py
+
+Введите ваш логин:  ваш логин
+Введите ваш пароль: ваш пароль
+Online:
+Фамилия Имя - user_id: 
+
 ```
 
 Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,17 @@
 import vk
+from getpass import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 6138740  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    login = input('Введите ваш логин: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass('Введите ваш пароль: ')
+    return password
 
 
 def get_online_friends(login, password):
@@ -17,13 +19,20 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
     # например, api.friends.get()
+    friends_online_id = api.friends.getOnline()
+    friends_online_name = api.users.get(user_ids=friends_online_id)
+    return friends_online_name
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('Online: ')
+    for friend in friends_online:
+        print('{} {} - user_id: {}'.format(friend['last_name'], friend['first_name'], friend['uid']))
+
 
 if __name__ == '__main__':
     login = get_user_login()
